455cbe4e97df9d85709d3c00d538e42c279e17a4
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-07-21 11:52:25 +0200

    Update Icons.tsx

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import icon from "../../Images/icons.svg";
-import icon2 from "../../../public/icons.svg";
+import icon2 from "/icons.svg";
 
 export function IconSearch() {
     console.log("Icon Paths:", icon2)
